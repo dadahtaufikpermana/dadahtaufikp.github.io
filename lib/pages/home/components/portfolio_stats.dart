e6f731dceed177f81ba9b2ba462bca415ac185c0
Dadah Taufik <dadahtaufik@refactory.id>
2023-06-08 12:19:08 +0700

    update situs web

--- lib/pages/home/components/portfolio_stats.dart
+++ lib/pages/home/components/portfolio_stats.dart
@@ -25,50 +25,48 @@ class PortfolioStats extends StatelessWidget {
   }
 
   Widget _buildUi(double width, BuildContext context) {
-    return Container(
-      child: ResponsiveWrapper(
-        maxWidth: width,
-        minWidth: width,
-        defaultScale: false,
-        child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraint) {
-            return Wrap(
-              spacing: 20.0,
-              runSpacing: 20.0,
-              children: stats.map((stat) {
-                return Container(
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  // Just use the helper here really
-                  width: ScreenHelper.isMobile(context)
-                      ? constraint.maxWidth / 2.0 - 20
-                      : (constraint.maxWidth / 4.0 - 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        stat.count,
-                        style: GoogleFonts.josefinSans(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 32.0,
-                        ),
+    return ResponsiveWrapper(
+      maxWidth: width,
+      minWidth: width,
+      defaultScale: false,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraint) {
+          return Wrap(
+            spacing: 20.0,
+            runSpacing: 20.0,
+            children: stats.map((stat) {
+              return Container(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                // Just use the helper here really
+                width: ScreenHelper.isMobile(context)
+                    ? constraint.maxWidth / 2.0 - 20
+                    : (constraint.maxWidth / 4.0 - 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      stat.count,
+                      style: GoogleFonts.josefinSans(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32.0,
                       ),
-                      const SizedBox(
-                        width: 10.0,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      stat.text,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        color: kCaptionColor,
                       ),
-                      Text(
-                        stat.text,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          color: kCaptionColor,
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            );
-          },
-        ),
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
